616fc43b98d77d6d88e79dda2b670f643f09f8d2
Max Patten <amanthus2535@gmail.com>
2019-06-28 18:05:53 -0700

    Built up array of people objects to compare against when a new user is added and provide similarity results, wrote up pseudocode for comparison logic

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,148 @@
-friend = [];
+friend = [
+    {
+        "name": "Thom",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzQ5tB8Rm7c8d46OM13c5DYQEZoT5AV55BWZOWz-M-rFS9IFQT",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            1,
+            3,
+            4,
+            4
+        ]
+    },
+    {
+        "name": "Julie",
+        "photo": "https://images.stockfreeimages.com/2112/sfi226w/21126966.jpg",
+        "scores": [
+            4,
+            2,
+            3,
+            5,
+            4,
+            2,
+            3,
+            5,
+            4,
+            2
+        ]
+    },
+    {
+        "name": "James",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvcQ6h-TR1tXssBqRQU07JZfK33HquY6SPhfDeDRT7edScLb6nvg",
+        "scores": [
+            3,
+            3,
+            2,
+            4,
+            3,
+            3,
+            5,
+            5,
+            4,
+            5
+        ]
+    },
+    {
+        "name": "Gillian",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRccn6Zzc0yiSZ02_1zoXg-P6KO4uqXS_lr0sw28bytfYMQBF8z",
+        "scores": [
+            2,
+            4,
+            1,
+            3,
+            4,
+            2,
+            3,
+            3,
+            3,
+            3
+        ]
+    },
+    {
+        "name": "Daniel",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfqlQXP-zYC8I8K7zhAdz0YFSybi3bn3ks9VNCwG00i72j5Pj0",
+        "scores": [
+            1,
+            5,
+            2,
+            3,
+            5,
+            1,
+            2,
+            2,
+            2,
+            2
+        ]
+    },
+    {
+        "name": "Liz",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR98UTMUdaLq1nzIw_y_qu7xVQvA04h6CHzbfP8zKcIyg6hJwcK0g",
+        "scores": [
+            2,
+            4,
+            3,
+            2,
+            4,
+            4,
+            2,
+            1,
+            5,
+            5
+        ]
+    },
+    {
+        "name": "Gautam",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGGg53ykD6-wKPIWgXqdpcCMaRFW2gj_XR1ZCuxlig76x_JTGLOQ",
+        "scores": [
+            3,
+            3,
+            4,
+            1,
+            3,
+            2,
+            3,
+            2,
+            5,
+            3
+        ]
+    },
+    {
+        "name": "Karen",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdCbWizkbzeorvXKFTtqnncw5RrrsfIUdAjhx2O6mDp43fr2Z1dA",
+        "scores": [
+            4,
+            2,
+            5,
+            2,
+            2,
+            3,
+            2,
+            5,
+            4,
+            2
+        ]
+    },
+    {
+        "name": "Steve",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRd61fgS3Hf81V_SMbLqIg-ClgsGyd8xa9mGWMY6duv3w2SuzAN",
+        "scores": [
+            5,
+            1,
+            4,
+            3,
+            3,
+            5,
+            1,
+            1,
+            3,
+            1
+        ]
+    }
+];
 
 module.exports = friend;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,8 +4,7 @@
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 
-const tableData = require("../data/tableData");
-const waitListData = require("../data/waitinglistData");
+const friends = require("../data/friends");
 
 
 // ===============================================================================
@@ -19,12 +18,16 @@ module.exports = (app) => {
   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
 
-  app.get("/api/tables", (req, res) => {
-    res.json(tableData);
+  app.get("/api/friends", (req, res) => {
+    res.json(friends);
   });
 
-  app.get("/api/waitlist", (req, res) => {
-    res.json(waitListData);
+  app.post("/api/friends", (req, res) => {
+    let newFriend = req.body
+    //compatability logic --  will use a loop to compare the array of values for the new addition to the array of values for each existing record, so a 2D loop? Loop through friends array, then at each index of loop1, we use loop2 to run through each friend's response values, compare to corresponding indexes of new friends responses, and store the absolute value of differences summed as a 'compatability' value. Maybe store that compatability value as in index as a new array that is populated by the first loop as it goes (after second loop builds up that compatability value for the individual). So when the loops are done we will have an array of compatability values with indexes that correspond to the person in the original array that the new person was compared to. We can then use another, seperate 1D array to flip through the compatability scores array and return the index with the lowest value. That best match case from the original friend array can be served up in the modal to the user, or at least saved in a variable to be used for that purpose, before the new friend is added to the total array of friends.
+
+
+    res.json(true);
   });
 
   // API POST Requests
